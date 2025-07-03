b7b4d5aa36b7aee2648b903af6d9a581df1ee79d
Mahmoud Mohamed <mahmoud.abdalaziz@outlook.com>
2025-07-03 18:19:56 +0300

    Add project files.

---
+++ SurveyBasket.Web/Program.cs
@@ -0,0 +1,38 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = "swagger";
+        options.SwaggerEndpoint("/openapi/v1.json", "TROJAN API");
+    });
+
+    // Scalar UI
+    app.MapScalarApiReference(options =>
+    {
+        options.OpenApiRoutePattern = "/openapi/v1.json"; // This should match the OpenAPI route pattern 
+        options.Title = "TROJAN API";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
